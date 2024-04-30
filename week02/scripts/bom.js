1c451af9c13c20d27b8f5543108afe406684a73f
ZigzagAlloy01 <ivanblued@gmail.com>
2024-04-29 23:10:20 -0600

    bom.js need some extra work

---
+++ week02/scripts/bom.js
@@ -0,0 +1,5 @@
+const input = document.getElementById('favchap');
+const button = document.querySelector('button');
+const list = document.getElementById('list');
+
+button.addEventListener('click', () => { console.log("Add Chapter button clicked!") });
